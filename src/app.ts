70be51e738cc3e9be943b195f3d8effd385df8e9
Jesus David Mejia Vergara <davidmejia-2000@outlook.com>
2026-05-14 22:58:06 -0500

    feat: Se agrega el bcryptjs

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import cors from 'cors';
 import express from 'express';
 import { JwtAdapter } from './adapters/jwt.adapter';
+import { hash } from 'bcryptjs';
+import { bcryptAdapter } from './adapters/bcrypt.adapter';
 
 const app = express();
 
@@ -27,4 +29,22 @@ app.post('/validateToken', (req, res) => {
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
+  testBycript();
 });
+
+function testBycript(){
+  const passwordTest1 = 'abc123';
+  const passwordTest2 = 'abc123';
+  const hashPasswordTest1 = bcryptAdapter.hash(passwordTest1);
+  const hashPasswordTest2 = bcryptAdapter.hash(passwordTest2);
+
+  const test = {
+    password1: passwordTest1,
+    password2: passwordTest2,
+    password1Hash: hashPasswordTest1,
+    password2Hash: hashPasswordTest2,
+    password1Compare: bcryptAdapter.compare(passwordTest1, hashPasswordTest1),
+    password2Compare: bcryptAdapter.compare(passwordTest2, hashPasswordTest2),
+  }
+  console.log('\nTest - Misma Password pero diferentes Hash', test);
+}
